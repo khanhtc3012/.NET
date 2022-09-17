f006c30981bf105ee5fc1ef66502a8c345d602ab
USER <khanhtran1254@gmail.com>
2022-09-17 13:20:25 +0700

    Add project files.

---
+++ WEB_API/Models/Product.cs
@@ -0,0 +1,16 @@
+﻿namespace WEB_API.Models
+#pragma warning disable CS8618 // Non-nullable field must contain a non-null value when exiting constructor. Consider declaring as nullable.
+{
+    public class Product
+    {
+        public int Id { get; set; }
+        public string Sku { get; set; }
+        public string Content { get; set; }
+        public float Price { get; set; }
+        public float? DiscountPrice { get; set; }
+        public bool IsActive { get; set; }
+        public string ImageUrl { get; set; }
+        public int ViewCount { get; set; }
+        public DateTime CreatedAt { get; set; }
+    }
+}
